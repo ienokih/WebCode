0f68e727abd5bc83daf5fdd20ab7e05e20ba224b
DaJaBeCoding <38948265+DaJaBeCoding@users.noreply.github.com>
2019-11-17 16:02:19 +0100

    test

--- sourceCode/main.js
+++ sourceCode/main.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//lool
+
 function loop() {
     loopContent();
     window.requestAnimationFrame(loop);
